bdc002c5d768020212785b94973429f6a03ae4d7
PVillela <pvillela@gmail.com>
2021-04-10 22:57:53 -0400

    Added copyright.

--- internal/sfl/user_get_current_sfl.go
+++ internal/sfl/user_get_current_sfl.go
@@ -1,13 +1,43 @@
+/*
+ *  Copyright © 2021 Paulo Villela. All rights reserved.
+ *  Use of this source code is governed by the Apache 2.0 license
+ *  that can be found in the LICENSE file.
+ */
+
 package sfl
 
 import (
+	"github.com/pvillela/go-foa-realworld/internal/fs"
 	"github.com/pvillela/go-foa-realworld/internal/rpc"
 )
 
 // UserGetCurrentSfl is the stereotype instance for the service flow that
 // returns the current user.
 type UserGetCurrentSfl struct {
+	UserGetByNameDaf fs.UserGetByNameDafT
+	UserGenTokenBf   fs.UserGenTokenBfT
 }
 
 // UserGetCurrentSflT is the function type instantiated by UserGetCurrentSfl.
-type UserGetCurrentSflT = func(username string) rpc.UserOut
+type UserGetCurrentSflT = func(username string) (rpc.UserOut, error)
+
+func (s UserGetCurrentSfl) Make() UserGetCurrentSflT {
+	return func(username string) (rpc.UserOut, error) {
+		user, _, err := s.UserGetByNameDaf(username)
+		if err != nil {
+			return rpc.UserOut{}, err
+		}
+
+		if user.Name != username {
+			return rpc.UserOut{}, fs.ErrUnauthorizedUser
+		}
+
+		token, err := s.UserGenTokenBf(user)
+		if err != nil {
+			return rpc.UserOut{}, err
+		}
+
+		userOut := rpc.UserOut{}.FromModel(user, token)
+		return userOut, err
+	}
+}
--- internal/sfl/user_register_sfl.go
+++ internal/sfl/user_register_sfl.go
@@ -1,3 +1,9 @@
+/*
+ *  Copyright © 2021 Paulo Villela. All rights reserved.
+ *  Use of this source code is governed by the Apache 2.0 license
+ *  that can be found in the LICENSE file.
+ */
+
 package sfl
 
 import (
@@ -5,11 +11,11 @@ import (
 	"github.com/pvillela/go-foa-realworld/internal/rpc"
 )
 
-// RegisterSflS contains the dependencies required for the construction of a
-// RegisterSfl. It represents the action of registering a user.
-type RegisterSflS struct {
+// UserRegisterSfl is the stereotype instance for the service flow that
+// It represents the action of registering a user.
+type UserRegisterSfl struct {
 }
 
-// RegisterSfl is the type of a function that takes an rpc.UserRegisterIn as input
+// UserRegisterSflT is the type of a function that takes an rpc.UserRegisterIn as input
 // and returns a model.User.
-type RegisterSfl = func(in rpc.UserRegisterIn) model.User
+type UserRegisterSflT = func(in rpc.UserRegisterIn) model.User
