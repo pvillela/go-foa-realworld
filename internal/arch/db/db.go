10319f008c8d11f6d11488c9de2f920239c0d3a0
PVillela <pvillela@gmail.com>
2021-04-19 06:13:05 -0400

    Combined RecCtx and Txn into one file db.go.

--- internal/arch/db/db.go
+++ internal/arch/db/db.go
@@ -11,3 +11,13 @@ package db
 type RecCtx interface {
 	Rc() interface{}
 }
+
+// Txn defines the abstract interface for transactions.
+type Txn interface {
+
+	// Validate returns an error if the transaction is not valid.
+	Validate() error
+
+	// End ends the transaction.
+	End()
+}
