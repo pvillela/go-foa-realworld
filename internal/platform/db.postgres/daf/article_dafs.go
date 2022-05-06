378c5a191d65cde03129ee385fac185b7fff125d
PVillela <pvillela@gmail.com>
2022-05-06 03:04:40 -0400

    Major commit -- Refactored and fixed article_dafs, including moving the helper functions to a separate article_dafs_utils.go file.

--- internal/platform/db.postgres/daf/article_dafs.go
+++ internal/platform/db.postgres/daf/article_dafs.go
@@ -9,18 +9,11 @@ package daf
 import (
 	"context"
 	"fmt"
-	"github.com/georgysavva/scany/pgxscan"
 	"github.com/jackc/pgx/v4"
 	"github.com/pvillela/go-foa-realworld/internal/arch/errx"
 	"github.com/pvillela/go-foa-realworld/internal/arch/util"
 	"github.com/pvillela/go-foa-realworld/internal/bf"
 	"github.com/pvillela/go-foa-realworld/internal/model"
-	"strings"
-)
-
-const (
-	limitDefault  = 20
-	offsetDefault = 0
 )
 
 // ArticleCreateDaf implements a stereotype instance of type
@@ -57,32 +50,26 @@ var ArticleGetBySlugDaf ArticleGetBySlugDafT = func(
 	currUserId uint,
 	slug string,
 ) (model.ArticlePlus, error) {
-	// This implementation uses the readArticles helper function. Although a simpler direct
-	// implementation is clearly possible, the required SQL query and data mapping logic are
-	// sufficiently complex that it is best to deal with those things only once.
-
-	// See selectJoin query string in function readArticles
+	// See mainArticlePlusQuery
 	whereTuples := []util.Tuple2[string, any]{
 		{"a.slug = $%d", slug},
-		{"ufa2.username IS NULL", nil}, // force null join
 	}
+	where, whereArgs := whereClauseFromTuples(2, whereTuples)
 
-	where, whereArgs := whereClauseFromTuples(whereTuples)
-
-	articlePluses, _, err := readArticles(ctx, tx, currUserId, where, nil, nil, whereArgs...)
+	results, err := readArticles(ctx, tx, currUserId, where, nil, nil, whereArgs...)
 	if err != nil {
 		return model.ArticlePlus{}, err
 	}
 
-	if len(articlePluses) == 0 {
+	if len(results) == 0 {
 		return model.ArticlePlus{}, bf.ErrArticleSlugNotFound.Make(nil, slug)
 	}
-	if len(articlePluses) > 1 {
+	if len(results) > 1 {
 		util.PanicOnError(errx.NewErrx(nil,
 			fmt.Sprintf("Found multiple articles with same slug '%v'", slug)))
 	}
 
-	return articlePluses[0], nil
+	return results[0], nil
 }
 
 // ArticleUpdateDaf implements a stereotype instance of type
@@ -134,20 +121,18 @@ var ArticlesFeedDaf ArticlesFeedDafT = func(
 	optLimit *int,
 	optOffset *int,
 ) ([]model.ArticlePlus, error) {
-	// See selectJoin query string in function readArticles
+	// See mainArticlePlusQuery
 	whereTuples := []util.Tuple2[string, any]{
 		{"fo.follower_id = $%d", currUserId},
-		{"ufa2.username IS NULL", nil}, // force null join
 	}
+	where, whereArgs := whereClauseFromTuples(2, whereTuples)
 
-	where, whereArgs := whereClauseFromTuples(whereTuples)
-
-	articlePluses, _, err := readArticles(ctx, tx, currUserId, where, optLimit, optOffset, whereArgs...)
+	results, err := readArticles(ctx, tx, currUserId, where, optLimit, optOffset, whereArgs...)
 	if err != nil {
 		return nil, err
 	}
 
-	return articlePluses, nil
+	return results, nil
 }
 
 // ArticlesListDafT implements a stereotype instance of type
@@ -158,246 +143,40 @@ var ArticlesListDaf ArticlesListDafT = func(
 	currUserId uint,
 	criteria model.ArticleCriteria,
 ) ([]model.ArticlePlus, error) {
-	// See selectJoin query string in function readArticles
-	whereTuples := make([]util.Tuple2[string, any], 0)
+	// See mainArticlePlusQuery
+	var join string
+	var joinArgs []any
+	if v := criteria.FavoritedBy; v != nil {
+		join = `
+		LEFT JOIN favorites fa1 ON a.id = fa1.article_id -- at most one due to below
+		LEFT JOIN users ufa1 ON ufa1.id = fa1.user_id AND ufa1.username = $2
+		`
+		joinArgs = []any{*v}
+	}
+
+	var whereTuples []util.Tuple2[string, any]
 	if v := criteria.Tag; v != nil {
 		whereTuples = append(whereTuples, util.Tuple2[string, any]{"t.name = $%d", *v})
 	}
 	if v := criteria.Author; v != nil {
 		whereTuples = append(whereTuples, util.Tuple2[string, any]{"ua.username = $%d", *v})
 	}
-	if v := criteria.FavoritedBy; v != nil {
-		whereTuples = append(whereTuples, util.Tuple2[string, any]{"ufa2.username = $%d", *v})
+	var initialIndex int
+	if len(join) == 0 {
+		initialIndex = 2
 	} else {
-		// force null join
-		whereTuples = append(whereTuples, util.Tuple2[string, any]{"ufa2.username IS NULL", nil})
+		initialIndex = 3
 	}
+	where, whereArgs := whereClauseFromTuples(initialIndex, whereTuples)
 
-	where, whereArgs := whereClauseFromTuples(whereTuples)
+	additionalSql := join + where
+	additionalArgs := append(joinArgs, whereArgs...)
 
-	articlePluses, _, err := readArticles(ctx, tx, currUserId, where, criteria.Limit, criteria.Offset,
-		whereArgs...)
+	results, err := readArticles(ctx, tx, currUserId, additionalSql, criteria.Limit, criteria.Offset,
+		additionalArgs...)
 	if err != nil {
 		return nil, err
 	}
 
-	return articlePluses, nil
-}
-
-/////////////////////
-// Helper functions
-
-// Helper to whereClauseFromTuples; do not use it directly.
-func whereClauseFromSlices(wheres []string, nullableWhereArgs []any) (where string, whereArgs []any) {
-	if len(wheres) == 0 {
-		return "", nullableWhereArgs
-	}
-
-	wheresWithParams := make([]string, len(wheres))
-	whereArgs = make([]any, 0, len(wheres))
-	idx := 0
-	for i, w := range wheres {
-		if nullableWhereArgs[i] != nil {
-			wheresWithParams[i] = fmt.Sprintf(w, idx+2)
-			whereArgs = append(whereArgs, nullableWhereArgs[i])
-			idx++
-		} else {
-			wheresWithParams[i] = wheres[i]
-		}
-	}
-
-	where = "WHERE " + strings.Join(wheresWithParams, " AND ")
-	return where, whereArgs
-}
-
-func whereClauseFromTuples(whereTuples []util.Tuple2[string, any]) (where string, whereArgs []any) {
-	wheres := make([]string, len(whereTuples))
-	nullableWhereArgs := make([]any, len(whereTuples))
-	for i, nv := range whereTuples {
-		wheres[i] = nv.X1
-		nullableWhereArgs[i] = nv.X2
-	}
-	return whereClauseFromSlices(wheres, nullableWhereArgs)
-}
-
-// readArticles queries for articles, returning slice of model.ArticlePlus and an error.
-// The first slice contains model.ArticlePlus items.
-// The second slice contains, for each item, a slice of the user IDs of all users that have
-// favorited the article with the same index in the first slice. This second slice is not
-// very useful, except maybe for debugging purposes.
-//
-// currUserId is the user ID of the currently logged-in user.
-//
-// where is a WHERE clause string that filters the query. The argument placeholders in `where`
-// must start with $2 as $1 is reserved for `currUserId`. The query, implemented inside this
-// function, provides a denormalized holistic view of articles by joining the tables: articles,
-// users (twice), favorites (twice), followings, and tags. The double joins are required to
-// support the where clauses from the DAFs that call this function.
-//
-// optLimit and optOffset are optional limit and offset parameters for the result set.
-//
-// whereArgs are arguments for the where clause.
-func readArticles(
-	ctx context.Context,
-	tx pgx.Tx,
-	currUserId uint,
-	where string,
-	optLimit *int,
-	optOffset *int,
-	whereArgs ...any,
-) ([]model.ArticlePlus, [][]uint, error) {
-	// Construct SQL
-	selectJoin := `
-	SELECT a.*, ua.username, ua.bio, ua.image, fa1.user_id as favorited, fa2.user_id as favorites_user_id, 
-		fo.follower_id as following, t.name 
-		FROM articles a
-	LEFT JOIN users ua ON a.author_id = ua.id
-	LEFT JOIN favorites fa1 ON a.id = fa1.article_id AND $1 = fa1.user_id -- no product effect
-	LEFT JOIN favorites fa2 ON a.id = fa2.article_id -- product effect
-	LEFT JOIN users ufa2 ON fa2.user_id = ufa2.id -- product effect, same as fa2
-	LEFT JOIN followings fo ON fo.follower_id = $1 AND a.author_id = fo.followee_id -- no product effect
-	LEFT JOIN article_tags at ON a.id = at.article_id -- product effect
-	LEFT JOIN tags t ON at.tag_id = t.id
-	`
-	orderBy := `
-	ORDER BY a.created_at DESC, t.name, favorites_user_id
-	`
-	sql := selectJoin + where + orderBy
-	limit := limitDefault
-	if optLimit != nil {
-		limit = *optLimit
-		sql += fmt.Sprintf(" LIMIT %d", limit)
-	}
-	offset := offsetDefault
-	if optOffset != nil {
-		offset = *optOffset
-		sql += fmt.Sprintf(" OFFSET %d", offset)
-	}
-
-	fmt.Println("Full sql:", sql)
-
-	// Retrieve rows
-	args := append([]any{currUserId}, whereArgs...)
-	rows, err := tx.Query(ctx, sql, args...)
-	if err != nil {
-		return nil, nil, errx.ErrxOf(err)
-	}
-	defer rows.Close()
-
-	// Data structures to receive data
-	type recordT struct {
-		Id              uint
-		Article         model.Article `db:""`
-		Profile         model.Profile `db:""`
-		Following       *uint
-		Favorited       *uint
-		Tag             *string `db:"name"`
-		FavoritesUserId *uint
-	}
-	type resultT struct {
-		articlePlus      model.ArticlePlus
-		favoritesUserIds []uint
-	}
-	results := []resultT{}
-	tags := []string{}
-	favoritesUserIds := []uint{}
-	var currRecord recordT
-	var prevRecord recordT
-
-	// Functions to put data in data structures
-
-	fresh := true // true when first encountering a new article
-
-	articleChanged := func() bool {
-		return !fresh && currRecord.Id != prevRecord.Id
-	}
-
-	tagChanged := func() bool {
-		return fresh ||
-			(currRecord.Tag == nil && prevRecord.Tag != nil) ||
-			(currRecord.Tag != nil && prevRecord.Tag == nil) ||
-			(currRecord.Tag != nil && prevRecord.Tag != nil &&
-				*currRecord.Tag != *prevRecord.Tag)
-	}
-
-	favoritesUserIdChanged := func() bool {
-		return fresh ||
-			(currRecord.FavoritesUserId == nil && prevRecord.FavoritesUserId != nil) ||
-			(currRecord.FavoritesUserId != nil && prevRecord.FavoritesUserId == nil) ||
-			(currRecord.FavoritesUserId != nil && prevRecord.FavoritesUserId == nil &&
-				*currRecord.FavoritesUserId != *prevRecord.FavoritesUserId)
-	}
-
-	// Move data into results slice
-	updateResults := func() {
-		ra := &prevRecord.Article
-		rp := &prevRecord.Profile
-		if prevRecord.Following != nil {
-			rp.Following = true
-		}
-		favorited := false
-		if prevRecord.Favorited != nil {
-			favorited = true
-		}
-		articlePlus := model.ArticlePlus{
-			Slug: ra.Slug,
-			Author: model.Profile{
-				Username:  rp.Username,
-				Bio:       rp.Bio,
-				Image:     rp.Image,
-				Following: rp.Following,
-			},
-			Title:          ra.Title,
-			Description:    ra.Description,
-			Body:           ra.Body,
-			TagList:        tags,
-			CreatedAt:      ra.CreatedAt,
-			UpdatedAt:      ra.UpdatedAt,
-			Favorited:      favorited,
-			FavoritesCount: ra.FavoritesCount,
-		}
-		result := resultT{
-			articlePlus:      articlePlus,
-			favoritesUserIds: favoritesUserIds,
-		}
-		results = append(results, result)
-		tags = []string{}
-		favoritesUserIds = []uint{}
-	}
-
-	// Main processing loop
-	for rows.Next() {
-		err = pgxscan.ScanRow(&currRecord, rows)
-		if err != nil {
-			return nil, nil, errx.ErrxOf(err)
-		}
-
-		if articleChanged() {
-			updateResults()
-			fresh = true
-		}
-
-		if tagChanged() && currRecord.Tag != nil {
-			tags = append(tags, *currRecord.Tag)
-		}
-
-		if favoritesUserIdChanged() && currRecord.FavoritesUserId != nil {
-			favoritesUserIds = append(favoritesUserIds, *currRecord.FavoritesUserId)
-		}
-
-		prevRecord = currRecord
-		fresh = false
-	}
-	if !fresh {
-		updateResults()
-	}
-
-	articlePluses := make([]model.ArticlePlus, len(results))
-	favoritesUserIdSliceSlice := make([][]uint, len(results))
-	for i, r := range results {
-		articlePluses[i] = r.articlePlus
-		favoritesUserIdSliceSlice[i] = r.favoritesUserIds
-	}
-
-	return articlePluses, favoritesUserIdSliceSlice, nil
+	return results, nil
 }
---
+++ internal/platform/db.postgres/daf/article_dafs_utils.go
@@ -0,0 +1,221 @@
+/*
+ * Copyright © 2022 Paulo Villela. All rights reserved.
+ * Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
+
+package daf
+
+import (
+	"context"
+	"fmt"
+	"github.com/georgysavva/scany/pgxscan"
+	"github.com/jackc/pgx/v4"
+	"github.com/pvillela/go-foa-realworld/internal/arch/errx"
+	"github.com/pvillela/go-foa-realworld/internal/arch/util"
+	"github.com/pvillela/go-foa-realworld/internal/model"
+	log "github.com/sirupsen/logrus"
+	"strings"
+)
+
+/////////////////////
+// Helper functions for Article DAFs
+
+const (
+	limitDefault  = 20
+	offsetDefault = 0
+)
+
+// whereClauseFromTuples returns a parameterized WHERE clause and corresponding arguments slice from
+// a slice of tuples.
+// Each tuple in the input slice contributes to the where clause.
+// The output arguments slice contains the second components of the tuples, skiping any nil values.
+// The placeholders in the WHERE clause are numbered starting with initialIndex.
+func whereClauseFromTuples(
+	initialIndex int,
+	whereTuples []util.Tuple2[string, any],
+) (where string, whereArgs []any) {
+	wheres := make([]string, len(whereTuples))
+	nullableWhereArgs := make([]any, len(whereTuples))
+	for i, nv := range whereTuples {
+		wheres[i] = nv.X1
+		nullableWhereArgs[i] = nv.X2
+	}
+	return whereClauseFromSlices(initialIndex, wheres, nullableWhereArgs)
+}
+
+// mainArticlePlusQuery defines a denormalized "view" that retrieves all model.ArticlePlus objects.
+// This "view" can be constrained by appending additional JOIN and/or WHERE clauses to it.
+var mainArticlePlusQuery = `
+	SELECT a.*, ua.username, ua.bio, ua.image, fa.user_id as favorited, fo.follower_id as following, t.name 
+		FROM articles a
+	LEFT JOIN users ua ON a.author_id = ua.id
+	LEFT JOIN favorites fa ON fa.article_id = a.id AND fa.user_id = $1 -- at most one
+	LEFT JOIN followings fo ON fo.follower_id = $1 AND fo.followee_id = a.author_id -- at most one
+	LEFT JOIN article_tags at ON at.article_id = a.id -- product effect
+	LEFT JOIN tags t ON t.id = at.tag_id 
+`
+
+// readArticles retrieves articles, returning a slice of model.ArticlePlus and an error.
+// It is used by the DAFs that retrieve articles.
+//
+// Parameters other than the obvious ctx and tx:
+//
+// currUserId is the user ID of the currently logged-in user.
+//
+// additionaSql contains JOINs and/OR a WHERE clause that are appended to mainArticlePlusQuery to
+// filter the query.
+// The argument placeholders in additionalSql must start with $2 as $1 is reserved for currUserId.
+//
+// optLimit and optOffset are optional limit and offset parameters for the result set.
+//
+// additionalArgs are arguments for additionalSql.
+func readArticles(
+	ctx context.Context,
+	tx pgx.Tx,
+	currUserId uint,
+	additionalSql string,
+	optLimit *int,
+	optOffset *int,
+	additionalArgs ...any,
+) ([]model.ArticlePlus, error) {
+	// Construct SQL
+	orderBy := `
+	ORDER BY a.created_at DESC, t.name
+	`
+	sql := mainArticlePlusQuery + additionalSql + orderBy
+	limit := limitDefault
+	if optLimit != nil {
+		limit = *optLimit
+		sql += fmt.Sprintf(" LIMIT %d", limit)
+	}
+	offset := offsetDefault
+	if optOffset != nil {
+		offset = *optOffset
+		sql += fmt.Sprintf(" OFFSET %d", offset)
+	}
+
+	log.Debug("Full sql:", sql)
+
+	// Retrieve rows
+	args := append([]any{currUserId}, additionalArgs...)
+	rows, err := tx.Query(ctx, sql, args...)
+	if err != nil {
+		return nil, errx.ErrxOf(err)
+	}
+	defer rows.Close()
+
+	// Data structures to receive data
+	type recordT struct {
+		Id        uint
+		Article   model.Article `db:""`
+		Profile   model.Profile `db:""`
+		Following *uint
+		Favorited *uint
+		Tag       *string `db:"name"`
+	}
+	var results []model.ArticlePlus
+	var tags []string
+	var currRecord recordT
+	var prevRecord recordT
+
+	// Functions to put data in data structures
+
+	fresh := true // true when first encountering a new article
+
+	articleChanged := func() bool {
+		return !fresh && currRecord.Id != prevRecord.Id
+	}
+
+	tagChanged := func() bool {
+		return fresh ||
+			(currRecord.Tag == nil && prevRecord.Tag != nil) ||
+			(currRecord.Tag != nil && prevRecord.Tag == nil) ||
+			(currRecord.Tag != nil && prevRecord.Tag != nil &&
+				*currRecord.Tag != *prevRecord.Tag)
+	}
+
+	// Move data into results slice
+	updateResults := func() {
+		ra := &prevRecord.Article
+		rp := &prevRecord.Profile
+		if prevRecord.Following != nil {
+			rp.Following = true
+		}
+		favorited := false
+		if prevRecord.Favorited != nil {
+			favorited = true
+		}
+		result := model.ArticlePlus{
+			Slug: ra.Slug,
+			Author: model.Profile{
+				Username:  rp.Username,
+				Bio:       rp.Bio,
+				Image:     rp.Image,
+				Following: rp.Following,
+			},
+			Title:          ra.Title,
+			Description:    ra.Description,
+			Body:           ra.Body,
+			TagList:        tags,
+			CreatedAt:      ra.CreatedAt,
+			UpdatedAt:      ra.UpdatedAt,
+			Favorited:      favorited,
+			FavoritesCount: ra.FavoritesCount,
+		}
+		results = append(results, result)
+		tags = []string{}
+	}
+
+	// Main processing loop
+	for rows.Next() {
+		err = pgxscan.ScanRow(&currRecord, rows)
+		if err != nil {
+			return nil, errx.ErrxOf(err)
+		}
+
+		if articleChanged() {
+			updateResults()
+			fresh = true
+		}
+
+		if tagChanged() && currRecord.Tag != nil {
+			tags = append(tags, *currRecord.Tag)
+		}
+
+		prevRecord = currRecord
+		fresh = false
+	}
+	if !fresh {
+		updateResults()
+	}
+
+	return results, nil
+}
+
+// Helper to whereClauseFromTuples; do not use it directly.
+func whereClauseFromSlices(
+	initialIndex int,
+	wheres []string,
+	nullableWhereArgs []any,
+) (where string, whereArgs []any) {
+	if len(wheres) == 0 {
+		return "", nullableWhereArgs
+	}
+
+	wheresWithParams := make([]string, len(wheres))
+	whereArgs = make([]any, 0, len(wheres))
+	idx := initialIndex
+	for i, w := range wheres {
+		if nullableWhereArgs[i] != nil {
+			wheresWithParams[i] = fmt.Sprintf(w, idx)
+			whereArgs = append(whereArgs, nullableWhereArgs[i])
+			idx++
+		} else {
+			wheresWithParams[i] = wheres[i]
+		}
+	}
+
+	where = "WHERE " + strings.Join(wheresWithParams, " AND ")
+	return where, whereArgs
+}
