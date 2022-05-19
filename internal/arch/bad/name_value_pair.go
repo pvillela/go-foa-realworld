bc89cb31673661d819863f24a509b00865d95435
PVillela <pvillela@gmail.com>
2022-05-19 09:56:59 -0400

    Deleted arch/bad directory.

--- internal/arch/bad/name_value_pair.go
+++
@@ -1,8 +0,0 @@
-package bad
-
-// There should be a syntax error highlight below saying:
-// "generic type cannot be alias [compiler(BadDecl)]"
-type NameValuePair[N any, V any] = struct {
-	Name  N
-	Value V
-}
